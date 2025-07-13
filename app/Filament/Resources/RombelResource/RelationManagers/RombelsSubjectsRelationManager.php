f45db8336fd0ec8c39d6cc94ef2bce5422ed18a6
ilham <ilhamafandi21@gmail.com>
2025-07-14 01:03:10 +0700

    Perbaikian validasi di rombel resource dan semua yg terlibat

--- app/Filament/Resources/RombelResource/RelationManagers/RombelsSubjectsRelationManager.php
+++ app/Filament/Resources/RombelResource/RelationManagers/RombelsSubjectsRelationManager.php
@@ -3,12 +3,13 @@
 namespace App\Filament\Resources\RombelResource\RelationManagers;
 
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class RombelsSubjectsRelationManager extends RelationManager
 {
@@ -43,7 +44,18 @@ class RombelsSubjectsRelationManager extends RelationManager
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DeleteAction::make()
+                    ->before(function ($record, $action) {
+                        if ($record->teachers()->exists()) {
+                            Notification::make()
+                                ->title('Gagal Hapus!')
+                                ->body('Masih ada guru yg terlibat mata pelajaran ini, silahkan hapus dari sisi Guru dan Mapel')
+                                ->danger()
+                                ->send();
+
+                            $action->cancel(); 
+                        }
+                    }),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([

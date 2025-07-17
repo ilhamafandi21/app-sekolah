36078f403b8b560ce5522a0c6896d2dd41f7cbbf
ilham <ilhamafandi21@gmail.com>
2025-07-18 01:37:58 +0700

    modified:   app/Filament/Resources/RombelResource/RelationManagers/SiswasRelationManager.php

--- app/Filament/Resources/RombelResource/RelationManagers/SiswasRelationManager.php
+++ app/Filament/Resources/RombelResource/RelationManagers/SiswasRelationManager.php
@@ -56,12 +56,13 @@ class SiswasRelationManager extends RelationManager
                 Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                // Tables\Actions\EditAction::make(),
+                // Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    // Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }

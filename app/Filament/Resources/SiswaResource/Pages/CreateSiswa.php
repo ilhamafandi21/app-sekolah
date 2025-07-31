5ec3ecf161e7d2ebe64a82037e5766143a39f953
ilham <ilhamafandi21@gmail.com>
2025-07-31 08:23:42 +0700

    menambah validasi create sebelum ada role

--- app/Filament/Resources/SiswaResource/Pages/CreateSiswa.php
+++ app/Filament/Resources/SiswaResource/Pages/CreateSiswa.php
@@ -15,22 +15,20 @@ class CreateSiswa extends CreateRecord
     protected static string $resource = SiswaResource::class;
 
 
-    protected function beforeCreate(array $data): array
+    protected function beforeCreate(): void
     {
-        // Pastikan role 'siswa' tersedia
+        // Cek apakah role 'siswa' tersedia
         $role = Role::where('name', 'siswa')->where('guard_name', 'web')->first();
 
         if (! $role) {
             Notification::make()
-                ->title('Role siswa belum dibuat.')
-                ->body("Role: {$role} tidak ada. Buat dulu!")
+                ->title('Role "siswa" belum tersedia.')
+                ->body('Silakan buat role "siswa" terlebih dahulu sebelum membuat data.')
                 ->danger()
                 ->send();
 
-                $this->halt();
-            }
-
-        return $data;
+            $this->halt(); // Hentikan proses simpan
+        }
     }
 
     protected function handleRecordCreation(array $data): \Illuminate\Database\Eloquent\Model
@@ -40,10 +38,9 @@ class CreateSiswa extends CreateRecord
             'name' => $data['name'],
             'email' => $data['user']['email'],
             'password' => Hash::make($data['user']['password']),
-        ])->assignRole('siswa'); // assogneRole ke Spatie
+        ])->assignRole('siswa');// assogneRole ke Spatie
 
         $data['user_id'] = $user->id;
-
 
         unset($data['user']['email'], $data['user']['password']);
 

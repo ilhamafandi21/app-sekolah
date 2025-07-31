a060da552239482b922b3c84cb4f584d4a94b64d
ilham <ilhamafandi21@gmail.com>
2025-08-01 02:46:20 +0700

    Kembali lagi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(2)->create();
         // Siswa::factory(200)->create();
-        Jurusan::factory(84)->create();
-        Subject::factory(50)->create();
+        // Jurusan::factory(84)->create();
+        // Subject::factory(50)->create();
         // Teacher::factory()->count(40)->create();
         // Semester::factory()->count(4)->create();
 

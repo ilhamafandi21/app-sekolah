0088ce046a0a419401e9528afa17f6c795bc5f75
ilham <ilhamafandi21@gmail.com>
2025-08-02 06:55:58 +0700

    Perbaikan tingkat

--- app/Filament/Resources/TingkatResource/Pages/EditTingkat.php
+++ app/Filament/Resources/TingkatResource/Pages/EditTingkat.php
@@ -16,4 +16,45 @@ class EditTingkat extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function mutateFormDataBeforeSave(array $data): array 
+    {  
+        $record = $this->record;
+
+        $original = $record->tahun_ajaran_id == $data['tahun_ajaran_id'] &&
+                    $record->tahun_ajaran_id == $data['nama_tingkat'];
+
+        if ($original) {
+            return $data;
+        }
+
+        $cekdataexists = \App\Models\Tingkat::where('tahun_ajaran_id', $data['tahun_ajaran_id'] )
+            ->where('nama_tingkat', $data['nama_tingkat'])
+            ->where('id', '!=' ,$record->id)
+            ->exists();
+
+        if ($cekdataexists) {
+
+            \Filament\Notifications\Notification::make()
+                ->title('Peringatan')
+                ->body('Kombinasi tahun ajaran dan tingkat sudah ada.')
+                ->danger()
+                ->send();
+
+            $this->halt();
+
+        } else {
+            $data['tahun_ajaran_id'];
+            $data['nama_tingkat'];
+            $data['keterangan'];
+
+            \Filament\Notifications\Notification::make()
+                ->title('Berhasil')
+                ->body('Data berhasil di ubah.')
+                ->success()
+                ->send();
+        }
+
+        return $data;
+    }
 }

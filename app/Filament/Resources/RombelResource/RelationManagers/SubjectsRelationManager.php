18ce5763efba3db576cc43a6c07c73a11f351843
ilham <ilhamafandi21@gmail.com>
2025-08-04 21:57:19 +0700

    Guru Resoource

--- app/Filament/Resources/RombelResource/RelationManagers/SubjectsRelationManager.php
+++ app/Filament/Resources/RombelResource/RelationManagers/SubjectsRelationManager.php
@@ -3,12 +3,13 @@
 namespace App\Filament\Resources\RombelResource\RelationManagers;
 
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use App\Models\Teacher;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class SubjectsRelationManager extends RelationManager
 {
@@ -45,6 +46,13 @@ class SubjectsRelationManager extends RelationManager
             ->actions([
                 Tables\Actions\DetachAction::make()
                     ->icon('heroicon-o-trash'),
+                Tables\Actions\Action::make('tambahTeacher')
+                    ->form([
+                         Forms\Components\Select::make('teacher_id')
+                            ->options(\App\Models\Teacher::pluck('name', 'id'))
+                            ->searchable()
+                            ->required(),
+                    ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([

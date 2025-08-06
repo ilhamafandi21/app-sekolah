cbc64898e8a8cf60f564d766ec5ed629687e37ba
ilham <ilhamafandi21@gmail.com>
2025-08-06 22:00:06 +0700

    Skejul

--- app/Filament/Resources/SchedullResource/Pages/CreateSchedull.php
+++ app/Filament/Resources/SchedullResource/Pages/CreateSchedull.php
@@ -2,11 +2,26 @@
 
 namespace App\Filament\Resources\SchedullResource\Pages;
 
-use App\Filament\Resources\SchedullResource;
 use Filament\Actions;
+use Illuminate\Support\Carbon;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\SchedullResource;
 
 class CreateSchedull extends CreateRecord
 {
     protected static string $resource = SchedullResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $hari = \App\Models\Day::find($data['day_id'])->nama_hari;
+        $startAt =  Carbon::parse($data['start_at'])->format('H:i');
+        $endAt =  Carbon::parse($data['end_at'])->format('H:i');
+
+        $kombinasi = $hari. '/' .$startAt. '/' .$endAt;
+
+        dd($kombinasi);
+
+
+        return $data;
+    }
 }

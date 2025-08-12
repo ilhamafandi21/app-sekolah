1ca9dcceb0e84c0d81fca5183f05f2218e101f82
ilham <ilhamafandi21@gmail.com>
2025-08-12 22:43:46 +0700

    Semester

--- app/Filament/Resources/RombelResource/RelationManagers/RombelsSubjectsSchedullsTeachersRelationManager.php
+++ app/Filament/Resources/RombelResource/RelationManagers/RombelsSubjectsSchedullsTeachersRelationManager.php
@@ -3,24 +3,94 @@
 namespace App\Filament\Resources\RombelResource\RelationManagers;
 
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use App\Models\Rombel;
+use App\Models\Schedull;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class RombelsSubjectsSchedullsTeachersRelationManager extends RelationManager
 {
     protected static string $relationship = 'RombelsSubjectsSchedullsTeachers';
 
     public function form(Form $form): Form
-    {
+   {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
+                Forms\Components\Select::make('rombel_id')
                     ->required()
-                    ->maxLength(255),
+                    ->relationship(
+                        name: 'rombel',
+                        titleAttribute: 'kode',
+                        modifyQueryUsing: fn (Builder $query) => $query
+                            ->with(['jurusan:id,nama_jurusan', 'tingkat:id,nama_tingkat'])
+                    )
+                    ->getOptionLabelFromRecordUsing(fn (Rombel $record) => sprintf(
+                        '%s || %d %s-%s',
+                        $record->kode,
+                        $record->tingkat->nama_tingkat,
+                        $record->jurusan->nama_jurusan,
+                        $record->divisi,
+                    ))
+                    ->reactive(),
+                Forms\Components\Select::make('subject_id')
+                    ->required()
+                    ->options(function (callable $get) {
+                        $rombelId = $get('rombel_id');
+                        if (!$rombelId) return [];
+                        $rombel = Rombel::with('subjects')->find($rombelId);
+                        return $rombel
+                            ? $rombel->subjects->pluck('name', 'id')->toArray()
+                            : [];
+                    })
+                    ->reactive()
+                    ->afterStateUpdated(function ($get, $set) {
+                        $rombelId = $get('rombel_id');
+                        $subjectId = $get('subject_id');
+
+                        if ($rombelId && $subjectId) {
+                            $pivot = \App\Models\RombelsSubjects::where('rombel_id', $rombelId)
+                                ->where('subject_id', $subjectId)
+                                ->first();
+                            if ($pivot) {
+                               $set('rombels_subjects_id', "$pivot->id");
+                            }
+                        }
+                        return [];
+                    }),
+               
+                Forms\Components\TextInput::make('rombels_subjects_id')
+                    ->label('Rombel Subjects ID')
+                    ->required()
+                    ->disabled()
+                    ->dehydrated()
+                    ->reactive()
+                    ->default(fn($get) => $get('subject_id')),
+               
+                Forms\Components\Select::make('schedull_id')
+                    ->required()
+                    ->relationship(
+                        name: 'schedull',
+                        titleAttribute: 'kode',
+                        modifyQueryUsing: fn (Builder $query) => $query
+                            ->select(['id','kode','start_at','end_at']) // batasi kolom (wajib sertakan 'id')
+                            ->orderBy('start_at')
+                    )
+                    ->getOptionLabelFromRecordUsing(function (Schedull $s) {
+                        $start = $s->start_at ? substr($s->start_at, 0, 5) : '-';
+                        $end   = $s->end_at   ? substr($s->end_at, 0, 5)   : '-';
+                        return "{$s->kode} — {$start} s/d {$end}";
+                    }),
+                
+                Forms\Components\Select::make('day_id')
+                    ->required()
+                    ->relationship('day', 'nama_hari'),    
+
+                Forms\Components\Select::make('teacher_id')
+                    ->relationship('teacher', 'name'),
             ]);
     }
 

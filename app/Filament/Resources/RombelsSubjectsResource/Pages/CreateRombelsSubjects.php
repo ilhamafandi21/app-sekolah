79772dcf32adc26f6881015cbcc8767264dc9c0d
ilham <ilhamafandi21@gmail.com>
2025-08-16 12:45:41 +0700

    8/16/2025

--- app/Filament/Resources/RombelsSubjectsResource/Pages/CreateRombelsSubjects.php
+++ app/Filament/Resources/RombelsSubjectsResource/Pages/CreateRombelsSubjects.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\RombelsSubjectsResource\Pages;
 
-use App\Filament\Resources\RombelsSubjectsResource;
 use Filament\Actions;
+use App\Models\RombelsSubjects;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\RombelsSubjectsResource;
 
 class CreateRombelsSubjects extends CreateRecord
 {
@@ -12,6 +14,21 @@ class CreateRombelsSubjects extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        
+            $exists = RombelsSubjects::query()
+                ->where('rombel_id', $data['rombel_id'] ?? null)
+                ->where('subject_id', $data['subject_id'] ?? null)
+                ->exists();
+
+            if ($exists) {
+                Notification::make()
+                    ->title('Data duplikat')
+                    ->body('Kombinasi Rombel & Subject sudah ada.')
+                    ->danger()
+                    ->send();
+
+                $this->halt(); // hentikan proses create
+            }
+
+            return $data;
     }
 }

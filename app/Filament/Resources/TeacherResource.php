beaf89985551fc5bd26d58f2fe15e1b1df3e8e8e
ilham <ilhamafandi21@gmail.com>
2025-08-17 07:44:58 +0700

    8/17/2025

--- app/Filament/Resources/TeacherResource.php
+++ app/Filament/Resources/TeacherResource.php
@@ -19,7 +19,7 @@ class TeacherResource extends Resource
 {
     protected static ?string $model = Teacher::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
     protected static ?string $navigationGroup = 'Akademik';
     protected static ?string $navigationLabel = 'Guru';
     protected static ?int $navigationSort = -10;

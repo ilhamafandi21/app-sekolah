7d138a276a204d99ef84b38f9d36172a30b8a44e
ilham <ilhamafandi21@gmail.com>
2025-08-17 07:40:07 +0700

    8/17/2025

--- app/Filament/Resources/JurusanResource.php
+++ app/Filament/Resources/JurusanResource.php
@@ -18,7 +18,7 @@ class JurusanResource extends Resource
 {
     protected static ?string $model = Jurusan::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-bookmark-square';
 
     public static function form(Form $form): Form
     {

fbb201a552f0cb0faf80a3ef0b5856a85e78b207
ilham <ilhamafandi21@gmail.com>
2025-08-21 01:01:19 +0700

    08/21/2025

--- app/Filament/Resources/RombelsPenilaianResource/Pages/CreateRombelsPenilaian.php
+++ app/Filament/Resources/RombelsPenilaianResource/Pages/CreateRombelsPenilaian.php
@@ -2,11 +2,41 @@
 
 namespace App\Filament\Resources\RombelsPenilaianResource\Pages;
 
-use App\Filament\Resources\RombelsPenilaianResource;
 use Filament\Actions;
+use App\Models\RombelsPenilaian;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\RombelsPenilaianResource;
 
 class CreateRombelsPenilaian extends CreateRecord
 {
     protected static string $resource = RombelsPenilaianResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        dd($data);
+         // --- KOMBINASI UMUM TANPA teacher_id (paling sering dipakai) ---
+        $exists = static::getModel()::query()
+            ->where('rombel_id',         $data['rombel_id'] ?? null)
+            ->where('siswa_id',          $data['siswa_id'] ?? null)
+            ->where('subject_id',        $data['subject_id'] ?? null)
+            ->where('indikatornilai_id', $data['indikatornilai_id'] ?? null)
+            ->where('semester_id',       $data['semester_id'] ?? null)
+            ->exists();
+
+        if ($exists) {
+            Notification::make()
+                ->title('Nilai sudah di input')
+                ->body('Kombinasi Rombel, Siswa, Subject, Indikator, dan Semester sudah gi input.')
+                ->danger()
+                ->send();
+
+            $this->halt(); // hentikan create
+        }
+
+        // (opsional) normalisasi/guard nilai
+        $data['nilai'] = isset($data['nilai']) ? (int) $data['nilai'] : null;
+
+        return $data;
+    } 
 }

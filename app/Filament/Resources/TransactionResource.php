60f3791a31e9e85d83c86234177b793927a7e08e
ilham <ilhamafandi21@gmail.com>
2025-08-24 23:00:07 +0700

    08/24/2025

---
+++ app/Filament/Resources/TransactionResource.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionResource\Pages;
+use App\Filament\Resources\TransactionResource\RelationManagers;
+use App\Models\Transaction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransactionResource extends Resource
+{
+    protected static ?string $model = Transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('kode')
+                    ->required(),
+                Forms\Components\TextInput::make('siswa_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('biaya_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('rombel_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('tingkat_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('jurusan_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('divisi')
+                    ->required(),
+                Forms\Components\TextInput::make('nominal')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('keterangan')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('kode')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('siswa_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('biaya_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('rombel_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('tingkat_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('jurusan_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('divisi')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('nominal')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('keterangan')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransaction::route('/create'),
+            'edit' => Pages\EditTransaction::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -17,4 +17,29 @@ class Transaction extends Model
         'nominal',
         'keterangan',
     ];
+
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class);
+    }
+
+    public function biaya()
+    {
+        return $this->belongsTo(Biaya::class);
+    }
+    public function rombel()
+    {
+        return $this->belongsTo(Rombel::class);
+    }
+    public function tingkat()
+    {
+        return $this->belongsTo(Tingkat::class);
+    }
+
+    public function jurusan()
+    {
+        return $this->belongsTo(Jurusan::class);
+    }
+
+
 }

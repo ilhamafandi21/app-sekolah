29d8faac1ebfde82e1d32b7c0ab5b7cadb74d1e7
ilham <ilhamafandi21@gmail.com>
2025-08-27 01:50:48 +0700

    08/27/2025

--- app/Filament/Resources/TeacherResource.php
+++ app/Filament/Resources/TeacherResource.php
@@ -40,9 +40,7 @@ class TeacherResource extends Resource
     protected static ?string $model = Teacher::class;
 
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-user-group';
-    protected static string | \UnitEnum | null $navigationGroup = 'Akademik';
-    protected static ?string $navigationLabel = 'Guru';
-    protected static ?int $navigationSort = -10;
+    protected static ?string $navigationLabel = 'Teacher';
 
 
         public static function form(Schema $schema): Schema

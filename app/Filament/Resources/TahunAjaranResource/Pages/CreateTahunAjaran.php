041bed88fc7861a80fbc9ab1e13d43e2c2708b73
ilham <ilhamafandi21@gmail.com>
2025-08-30 08:14:49 +0700

    08/30/2025

--- app/Filament/Resources/TahunAjaranResource/Pages/CreateTahunAjaran.php
+++ app/Filament/Resources/TahunAjaranResource/Pages/CreateTahunAjaran.php
@@ -2,13 +2,17 @@
 
 namespace App\Filament\Resources\TahunAjaranResource\Pages;
 
-use App\Filament\Resources\TahunAjaranResource;
-use Filament\Actions;
+
+
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\TahunAjaranResource;
 
 class CreateTahunAjaran extends CreateRecord
 {
     protected static string $resource = TahunAjaranResource::class;
     protected static ?string $title = 'Buat Tahun Ajaran Baru';
-
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }

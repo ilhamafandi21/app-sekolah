fdcc16b6cf6104be11fed44271e7f116fe35bfd1
ilham <ilhamafandi21@gmail.com>
2025-08-30 07:52:56 +0700

    08/30/2025

--- app/Filament/Resources/TahunAjaranResource/Pages/ListTahunAjarans.php
+++ app/Filament/Resources/TahunAjaranResource/Pages/ListTahunAjarans.php
@@ -2,22 +2,26 @@
 
 namespace App\Filament\Resources\TahunAjaranResource\Pages;
 
-use Filament\Actions\CreateAction;
-use App\Filament\Resources\TahunAjaranResource;
 use Filament\Actions;
 use Filament\Actions\ActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Support\Enums\IconPosition;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\TahunAjaranResource;
 
 class ListTahunAjarans extends ListRecords
 {
     protected static string $resource = TahunAjaranResource::class;
-    protected static ?string $title = 'Data Tahun Ajaran';
+    protected static ?string $title = 'Daftar Tahun Ajaran';
 
 
     protected function getHeaderActions(): array
     {
         return [
-                CreateAction::make(),
+                CreateAction::make()
+                    ->label('Buat')
+                    ->icon('heroicon-o-plus-circle')
+                    ->iconPosition(IconPosition::After)
         ];
     }
 }

55bf843b2807aa62584edb61f11cea5f38f6dde4
ilham <ilhamafandi21@gmail.com>
2025-09-05 21:32:59 +0700

    05/09/2025

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -33,7 +33,7 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-rectangle-stack';
-    protected static string | \UnitEnum | null $navigationGroup = 'Manaen';
+    protected static string | \UnitEnum | null $navigationGroup = 'Man';
 
     public static function form(Schema $schema): Schema
     {

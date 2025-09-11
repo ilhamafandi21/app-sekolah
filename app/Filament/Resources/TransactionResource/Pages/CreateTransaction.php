0dd2ad305a7da38bb9938b4f9abc0e49cc9549ab
ilham <ilhamafandi21@gmail.com>
2025-09-11 22:02:19 +0700

    11-09-2026

--- app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
+++ app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\TransactionResource\Pages;
 
 use App\Filament\Resources\TransactionResource;
+use App\Models\Siswa;
+use App\Models\SiswaBiaya;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
@@ -13,6 +15,7 @@ class CreateTransaction extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
+        dd($data);
         $data['kode'] = $data['rombel_id'] .
             $data['biaya_id'] .
             $data['siswa_id'] .
@@ -20,6 +23,12 @@ class CreateTransaction extends CreateRecord
             $data['jurusan_id'] .
             $data['divisi'] .
             date('YmdHis');
+
+            SiswaBiaya::create([
+                'siswa_id' => $data['siswa_id'],
+                'biaya_id' => $data['biaya_id'],
+
+            ]);
 
         return $data;
     }

8e2d25522ffe8943002f19946d3201b149cc371f
ilham <ilhamafandi21@gmail.com>
2025-09-16 00:04:54 +0700

    09-16-2025

--- app/Policies/RombelPolicy.php
+++ app/Policies/RombelPolicy.php
@@ -120,7 +120,7 @@ class RombelPolicy
         }
 
         // Staff hanya boleh kalau position = operational
-        if ($user->hasRole('staff') && $user->staff?->position === '') {
+        if ($user->hasRole('staff') && $user->staff?->position === 'operasional') {
             return true;
         }
 
